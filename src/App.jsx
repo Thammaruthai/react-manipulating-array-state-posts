e035f7df33e9c7e385407ee5caff218424de80d7
podusit <d.rimdusit@gmail.com>
2024-10-24 15:23:56 +0700

    feat:add like and dislike button for posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import Posts from "./components/Posts";
 
-function App() {
-  return (
-    <div className="App">
-      <Posts />
-    </div>
-  );
-}
 
+function App() {
+return (
+  <div className="App">
+    <Posts />
+  </div>
+);
+}
 export default App;
